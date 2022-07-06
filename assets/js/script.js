dcf489a460736029c39f7efc0c2818a05683a97b
AsmaaAamir <asma.liaqat@outlook.com>
2022-07-06 10:16:12 +0000

    slowed down the frame render timer for better game animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,32 @@
+// Variables and Constants // 
+
+
+let direction ={x:0, y:0}; 
+let speed = 2;
+let lastRenderTime = 0; 
+
+
+
+// Frame Rending 
+function game(currentTime){
+    window.requestAnimationFrame(game);
+    console.log(currentTime);
+    if ((currentTime - lastRenderTime)/1000 < 1/speed){
+        return;
+    } else 
+    lastRenderTime = currentTime
+    runGame();    
+}
+ function runGame(){
+    
+ }
+
+
+
+window.requestAnimationFrame(game);
+
+
+
 function direction (){
 
 }
