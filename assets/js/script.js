29c750f59c9c3e7d7e726bee93df8b9bfedf9a9c
AsmaaAamir <asma.liaqat@outlook.com>
2022-07-07 11:50:04 +0000

    fixed the food eating but wont eat the second food

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,7 @@ function runGame(){
 //Snake eating food and new locotion for food
     if (snakeBody[0].y === food.y && snakeBody[0].x === food.x){
         snakeBody.unshift({x: snakeBody[0].x + snakeDirection.x, y: snakeBody[0].y + snakeDirection.y});
-    
+        food = {x:Math.floor(Math.random()) ,y:Math.floor(Math.random())}
     }
 
 
