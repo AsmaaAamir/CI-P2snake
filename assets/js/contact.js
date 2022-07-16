cd2950331597faa3209019179b9de9339e9b3875
AsmaaAamir <asma.liaqat@outlook.com>
2022-07-16 13:54:55 +0000

    trying ot fix the email js issu

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,16 +1,13 @@
-(function() {
-    // https://dashboard.emailjs.com/admin/account
-    emailjs.init('VdTBCASn-iVtZo3xC');
-})();
+let sendButton = document.getElementById('btnSubmit');
 
-window.onload = function () {
     document.getElementById('contact-form').addEventListener('submit', function(event) {
         event.preventDefault();
-        emailjs.sendForm('contact_service', 'contact_form', this)
-        .then (function(){
-            console.log('Succss!!');
+        emailjs.init('user_VdTBCASn-iVtZo3xC');
+        emailjs.sendForm('snakegame', 'contact_form', this)
+        .then (()=> {
+            sendButton.balue ="send";
         }, function(error) {
             console.log('Failed...', error);
         });
-        });
-    }
+    });
+    
