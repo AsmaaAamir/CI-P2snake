4d1d1820cb4fd24b5f7a75599991b5c5e1975d7f
AsmaaAamir <asma.liaqat@outlook.com>
2022-07-16 14:54:02 +0000

    email js working

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,5 +1,4 @@
-
-
+// contact us EmailJS from -https://www.emailjs.com/docs/tutorial/creating-contact-form/ 
 window.onload =function(){
     document.getElementById('contact-form').addEventListener("submit", function (event) {
         event.preventDefault();
