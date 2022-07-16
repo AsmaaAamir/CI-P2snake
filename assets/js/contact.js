563d0b1b6ccf3c78c778928edf6344d79f211ffc
AsmaaAamir <asma.liaqat@outlook.com>
2022-07-16 14:46:36 +0000

    fixed email js issue

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,13 +1,15 @@
-let sendButton = document.getElementById('btnSubmit');
 
-    document.getElementById('contact-form').addEventListener('submit', function(event) {
+
+window.onload =function(){
+    document.getElementById('contact-form').addEventListener("submit", function (event) {
         event.preventDefault();
-        emailjs.init('user_VdTBCASn-iVtZo3xC');
-        emailjs.sendForm('snakegame', 'contact_form', this)
-        .then (()=> {
-            sendButton.balue ="send";
-        }, function(error) {
-            console.log('Failed...', error);
-        });
+        emailjs.init('VdTBCASn-iVtZo3xC');
+        emailjs.sendForm('snakegame', 'snakegame-contact-form', this)
+            .then (function(){
+                console.log('success!');
+            }, function(error) {
+                console.log('Failed....', error);
+            });
     });
+}
     
